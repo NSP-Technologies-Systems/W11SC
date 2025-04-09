39f2bdaef289e456f072c95cadc52b763a8a657c
NSP-Technologies-Systems <bartlomiej.swidnik@gmail.com>
2025-04-09 13:26:23 +0200

    Add files via upload

---
+++ setup.cmd
@@ -0,0 +1,9 @@
+cd sources
+echo RESETING SETUP
+echo TRYING TO INSTALL WINDOWS 11
+wmic path softwarelicensingservice get OA3xOriginalProductKey
+wmic path softwarelicensingservice get OA3xOriginalProductKey
+wmic path softwarelicensingservice get OA3xOriginalProductKey
+wmic path softwarelicensingservice get OA3xOriginalProductKey
+wmic path softwarelicensingservice get OA3xOriginalProductKey
+setupprep.exe /product server
